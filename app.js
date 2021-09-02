43ad889dee708b447bd6c004de11daf3a1715c3b
Rashmi <001rashmishukla@gmail.com>
2021-09-02 12:51:17 +0530

    add home page to project

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express();
 require('dotenv').config();
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.get("/",function(req,res){
   res.sendFile(__dirname+"/index.html");
